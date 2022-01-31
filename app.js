e5648ff0e952e9115634d732c6af0448cf0d790e
Riley Hays <haysrg@miamioh.edu>
2022-01-30 20:55:18 -0500

    Added try catch blocks to all my routes to handle errors

--- app.js
+++ app.js
@@ -11,21 +11,33 @@ app.get('/greetings', (req, res)=> {
 
 // Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', async (req, res)=> {
+  try{
 const quotes = await records.getQuotes();
 res.json(quotes);
+  } catch(err){
+    res.json({message: err.message});
+  }
 });
 // Send a GET request to /quotes/:id READ(view) a quote
 app.get('/quotes/:id', async (req, res)=> {
+  try{
 const quote = await records.getQuote(req.params.id);
 res.json(quote);
+  } catch(err){
+    res.json({message: err.message});
+  }
 });
 // Send a POST request to /quotes to CREATE a new quote
 app.post('/quotes', async (req, res)=>{
+  try{
    const quote = await records.createQuote({
       quote: req.body.quote,
       author: req.body.author
     });
     res.json(quote);
+  } catch(err){
+    res.json({message: err.message});
+  }
 })
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
 
