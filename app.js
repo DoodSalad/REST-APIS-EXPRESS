b0828064b4f3ddc73db861d9ef828ecd728fab29
Riley Hays <haysrg@miamioh.edu>
2022-01-31 00:25:37 -0500

    Wrote the update method using app.put() So that put requests will change the data of specific quotes in our data.json file

--- app.js
+++ app.js
@@ -22,24 +22,48 @@ res.json(quotes);
 app.get('/quotes/:id', async (req, res)=> {
   try{
 const quote = await records.getQuote(req.params.id);
-res.json(quote);
+if(quote){
+  res.json(quote);
+} else {
+  res.status(404).json({message: "Quote not found."});
+}
   } catch(err){
-    res.json({message: err.message});
+    res.status(500).json({message: err.message});
   }
 });
 // Send a POST request to /quotes to CREATE a new quote
 app.post('/quotes', async (req, res)=>{
   try{
-   const quote = await records.createQuote({
-      quote: req.body.quote,
-      author: req.body.author
-    });
-    res.json(quote);
+    if(req.body.author && req.body.quote){
+      const quote = await records.createQuote({
+        quote: req.body.quote,
+        author: req.body.author
+      });
+      res.status(201).json(quote);
+    } else {
+      res.status(400).json({message: "Quote and Author required."})
+    }
   } catch(err){
-    res.json({message: err.message});
+    res.status(500).json({message: err.message});
   }
 })
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
+app.put('/quotes/:id', async (req, res)=> {
+  try{
+    const quote = await records.getQuote(req.params.id);
+    if(quote){
+      quote.quote = req.body.quote;
+      quote.author = req.body.author;
+
+      await records.updateQuote(quote);
+      res.status(204).end();
+    } else {
+      res.status(404).json({message: "Quote wasn't found."});
+    }
+  } catch(err){
+    res.status(500).json({message: err.message});
+  }
+})
 
 // Send a DELETE request to /quotes/:id DELETE a quote
 
